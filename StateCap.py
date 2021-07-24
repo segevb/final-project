49f2779c84f5a9af4d05d790777e398a2067f947
Segev Bar-On <73795011+segevb@users.noreply.github.com>
2021-07-24 15:02:54 +0300

    Update StateCap.py

--- StateCap.py
+++ StateCap.py
@@ -9,12 +9,9 @@ is it in?
 Implement the function def get_state(capital): below so it returns the state.
 GOTCHAS: What happens if two states have the same capital name, how do you
 handle that?
-
 """
 import sys
-
 import pytest
-
 STATES_CAPITALS = {
     'Alabama' : 'Montgomery',
     'Alaska' : 'Juneau',
@@ -67,52 +64,52 @@ STATES_CAPITALS = {
     'Wisconsin' : 'Madison',
     'Wyoming' : 'Cheyenne',
 }
-
-
 def capital_of_Idaho():
     # Your code here
-    pass
-
+    return STATES_CAPITALS['Idaho']
+print(capital_of_Idaho())
 def all_states():
     # Your code here
-    pass
-
+   return STATES_CAPITALS.keys()
+print(all_states())
 def all_capitals():
     # Your code here
-    pass
-
+    return STATES_CAPITALS.values()
+print(all_capitals())
 def states_capitals_string():
     # Your code here
-    pass
-
-
-
+    single_string = ""
+    for key,value in STATES_CAPITALS.items():
+        single_string += key + " -> " + value + ", "
+    return single_string[:-2]
+print(states_capitals_string())
+def test_sorted(single_string):
+    dict1 = {}
+    for state_pair in single_string.split(", "):
+        members = state_pair.split(" -> ")
+        dict1.update({members[0] : members[1]})
+    states = list(dict1.keys())
+    for index in range(len(states) -1 ):
+        if states[index] < states[index + 1]:
+            continue
+        else:
+            print("string is not sorted alfabeteclly")
+            return
+    print("string is sorted")
+test_sorted(states_capitals_string())
 def get_state(capital):
     pass
-
-
-
 def test_state_to_capital():
     assert 'Cheyenne' == STATES_CAPITALS['Wyoming']
-
-
 def test_state_to_capital_unknown():
     with pytest.raises(KeyError):
         STATES_CAPITALS['']
-
-
 def test_capital_to_state():
     assert 'Wyoming' == get_state('Cheyenne')
-
-
 def test_capital_to_state_unknown():
     with pytest.raises(KeyError):
         get_state('')
-
-
-def main():
-    return pytest.main(__file__)
-
-
-if __name__ == '__main__':
-    sys.exit(main())
+# def main():
+#     return pytest.main(__file__)
+# if __name__ == '__main__':
+#     sys.exit(main())
